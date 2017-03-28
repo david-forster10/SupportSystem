e990078fe299d58d16922f373492c27d1473a8db
david-forster10 <dforster10@hotmail.com>
2017-03-28 13:40:17 +0100

    Added masked Password & Text field clearing on wrong input

--- Code/HelpLine/src/helpline/Login_Form.java
+++ Code/HelpLine/src/helpline/Login_Form.java
@@ -20,8 +20,8 @@ public class Login_Form extends javax.swing.JFrame {
         btn_Quit = new javax.swing.JButton();
         txt_Username = new javax.swing.JTextField();
         lbl_Password = new javax.swing.JLabel();
-        txt_Password = new javax.swing.JTextField();
         lbl_HelpLine = new javax.swing.JLabel();
+        txt_Password = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -59,12 +59,6 @@ public class Login_Form extends javax.swing.JFrame {
 
         lbl_Password.setText("Password");
 
-        txt_Password.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txt_PasswordActionPerformed(evt);
-            }
-        });
-
         lbl_HelpLine.setFont(new java.awt.Font("Tahoma", 0, 21)); // NOI18N
         lbl_HelpLine.setText("HelpLine");
 
@@ -73,7 +67,7 @@ public class Login_Form extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(171, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(btn_Go, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -91,17 +85,17 @@ public class Login_Form extends javax.swing.JFrame {
                         .addComponent(btn_Quit, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(2, 2, 2)))
                 .addGap(25, 25, 25)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txt_Username, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txt_Password, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(105, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(txt_Username, javax.swing.GroupLayout.DEFAULT_SIZE, 162, Short.MAX_VALUE)
+                    .addComponent(txt_Password))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(32, 32, 32)
                 .addComponent(lbl_HelpLine)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 62, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbl_Username)
                     .addComponent(txt_Username, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -109,7 +103,7 @@ public class Login_Form extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbl_Password)
                     .addComponent(txt_Password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(67, 67, 67)
+                .addGap(64, 64, 64)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btn_Go)
                     .addComponent(btn_Quit))
@@ -137,10 +131,6 @@ public class Login_Form extends javax.swing.JFrame {
 
     private void btn_GoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_GoActionPerformed
     }//GEN-LAST:event_btn_GoActionPerformed
-
-    private void txt_PasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_PasswordActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txt_PasswordActionPerformed
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {
     }
@@ -180,10 +170,14 @@ public class Login_Form extends javax.swing.JFrame {
             {
                 case 0:
                     JOptionPane.showMessageDialog(null, "Please Enter Valid Login Details! (2 Tries Left)", "Error", JOptionPane.WARNING_MESSAGE);
+                    txt_Username.setText("");
+                    txt_Password.setText("");
                     attempt =+ 1;
                     break;
                 case 1:
                     JOptionPane.showMessageDialog(null, "Please Enter Valid Login Details! (1 Try Left)", "Error", JOptionPane.WARNING_MESSAGE);
+                    txt_Username.setText("");
+                    txt_Password.setText("");
                     attempt =+ 1;
                     break;
                 case 2:
@@ -233,7 +227,7 @@ public class Login_Form extends javax.swing.JFrame {
     private javax.swing.JLabel lbl_HelpLine;
     private javax.swing.JLabel lbl_Password;
     private javax.swing.JLabel lbl_Username;
-    private javax.swing.JTextField txt_Password;
+    private javax.swing.JPasswordField txt_Password;
     private javax.swing.JTextField txt_Username;
     // End of variables declaration//GEN-END:variables
 }
